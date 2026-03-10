cf9632c45480d2b44a4b5196590e68b53797b377
DamienA <alves.d24@laposte.net>
2026-03-10 12:04:49 +0100

    refactor(build.gradle): change config sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,6 @@ sonarqube {
         property("sonar.projectKey", "DamienA24_p15-openclassroom")
         property("sonar.organization", "damiena24")
         property("sonar.host.url", "https://sonarcloud.io")
-        property("sonar.sources", "app/src/main/java")
-        property("sonar.tests", listOf("app/src/test/java", "app/src/androidTest/java"))
         property("sonar.coverage.jacoco.xmlReportPaths", "app/build/reports/jacoco/jacocoTestReport/jacocoTestReport.xml")
         property("sonar.java.binaries", "app/build/tmp/kotlin-classes/debug")
         property("sonar.exclusions", listOf(
