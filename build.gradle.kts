96ccb083b1c5d0bde3a5ac81d9029f5f5f94eb41
DamienA <alves.d24@laposte.net>
2026-03-10 12:42:16 +0100

    chore(sonar): change config

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ sonarqube {
         property("sonar.host.url", "https://sonarcloud.io")
         property("sonar.coverage.jacoco.xmlReportPaths", "app/build/reports/jacoco/jacocoTestReport/jacocoTestReport.xml")
         property("sonar.java.binaries", "app/build/tmp/kotlin-classes/debug")
+        property("sonar.qualitygate.wait", "true")
         property("sonar.exclusions", listOf(
             "**/R.class", "**/R\$*.class", "**/BuildConfig.*", "**/Manifest*.*",
             "**/*_MembersInjector.*", "**/Dagger*Component*.*", "**/*_Factory.*", "**/ComposableSingletons*"
